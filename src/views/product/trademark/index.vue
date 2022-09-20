d7e60c58da4e39fe1810cc95c6ce833399c953bb
黄天运 <9704880+huang-tianyun7758@user.noreply.gitee.com>
2022-09-20 14:17:25 +0800

    品牌数据的渲染

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -2,11 +2,18 @@
   <el-card>
     <el-button type="primary" :icon="Plus" class="mb-20">添加</el-button>
     <!-- 表格部分开始 -->
-    <el-table :data="[{}]" border>
-      <el-table-column type="index" label="序号" width="60" align="center" />
-      <el-table-column label="品牌名称" width="180" />
-      <el-table-column label="品牌LOGO" />
-      <el-table-column width="120" label="操作">
+    <el-table :data="trdemarkList" border>
+      <el-table-column type="index" label="序号" width="80" align="center" />
+      <el-table-column label="品牌名称" prop="tmName" />
+      <el-table-column label="品牌LOGO">
+        <!-- 
+           要渲染其他结构，需要使用默认插槽,作用域插槽，可以得到渲染的每条数据（第一行渲染就是数组第一条数据)
+         -->
+        <template #default="{ row }">
+          <img :src="row.logoUrl" class="logoImg" :alt="row.tmName" />
+        </template>
+      </el-table-column>
+      <el-table-column width="180" label="操作">
         <el-button :icon="Edit" class="options-btn" type="warning"></el-button>
         <el-button :icon="Delete" class="options-btn" type="danger"></el-button>
       </el-table-column>
@@ -43,11 +50,20 @@ export default {
 
 <script lang="ts" setup>
 import { Plus, Edit, Delete } from "@element-plus/icons-vue";
-import { ref } from "vue";
-
-const currentPage = ref(4); //当前页
-const pageSize = ref(100); //每页大小
-
+import { onMounted, ref } from "vue";
+//品牌管理相关的接口函数
+import { reqGetTrademarkList } from "@/api/product/trdemark";
+import type { trademarkList } from "@/api/product/modules/trademarkModel";
+/*****************************************分页器相关*************************************** */
+const currentPage = ref(1); //当前页
+const pageSize = ref(3); //每页记录数
+//品牌分页列表数据
+const trdemarkList = ref<trademarkList>([]);
+onMounted(async () => {
+  //发送请求获取数据
+  const res = await reqGetTrademarkList(currentPage.value, pageSize.value);
+  trdemarkList.value = res.records;
+});
 //每页显示条目个数变化触发的事件回调
 const handleSizeChange = (val: number) => {
   console.log(`${val} items per page`);
@@ -56,11 +72,16 @@ const handleSizeChange = (val: number) => {
 const handleCurrentChange = (val: number) => {
   console.log(`current page: ${val}`);
 };
+/*****************************************分页器相关*************************************** */
 </script>
 
 <style scoped>
 .options-btn {
   width: 30px;
   height: 25px;
+}
+.logoImg {
+  width: 100px;
+  height: 70px;
 }
 </style>
