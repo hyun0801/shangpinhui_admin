a251f50998c5a744d3eb9938f5957568e2290609
黄天运 <9704880+huang-tianyun7758@user.noreply.gitee.com>
2022-09-20 13:32:11 +0800

    品牌管理静态结构的搭建

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>trademark</div>
+  <el-card>
+    <el-button type="primary" :icon="Plus" class="mb-20">添加</el-button>
+    <!-- 表格部分开始 -->
+    <el-table :data="[{}]" border>
+      <el-table-column type="index" label="序号" width="60" align="center" />
+      <el-table-column label="品牌名称" width="180" />
+      <el-table-column label="品牌LOGO" />
+      <el-table-column width="120" label="操作">
+        <el-button :icon="Edit" class="options-btn" type="warning"></el-button>
+        <el-button :icon="Delete" class="options-btn" type="danger"></el-button>
+      </el-table-column>
+    </el-table>
+    <!-- 表格部分结束 -->
+    <!-- 分页器开始 -->
+    <!-- 
+        layout:布局方式
+            prev,next 上一页下一页
+            pager:分页序号
+            jumper:前往的页数
+            sizes:每页大小
+            total:数据总条数
+     -->
+    <el-pagination
+      class="mt-20"
+      v-model:currentPage="currentPage"
+      v-model:page-size="pageSize"
+      :page-sizes="[3, 6, 9, 12]"
+      layout="prev,pager,next,jumper,->,sizes ,total "
+      :total="100"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
+    <!-- 分页器结束 -->
+  </el-card>
 </template>
 
 <script lang="ts">
@@ -8,6 +41,26 @@ export default {
 };
 </script>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Plus, Edit, Delete } from "@element-plus/icons-vue";
+import { ref } from "vue";
 
-<style scoped></style>
+const currentPage = ref(4); //当前页
+const pageSize = ref(100); //每页大小
+
+//每页显示条目个数变化触发的事件回调
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+//当前页变化触发的事件回调
+const handleCurrentChange = (val: number) => {
+  console.log(`current page: ${val}`);
+};
+</script>
+
+<style scoped>
+.options-btn {
+  width: 30px;
+  height: 25px;
+}
+</style>
