bc7f13cc1b1fd71442e364011c9eb0f8050690a6
黄天运 <9704880+huang-tianyun7758@user.noreply.gitee.com>
2022-09-20 22:17:04 +0800

    category静态结构的搭建

---
+++ src/components/Category/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card>
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item label="一级分类">
+        <el-select placeholder="一级分类">
+          <el-option label="1" value="1" />
+          <el-option label="2" value="2" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select placeholder="二级分类">
+          <el-option label="1" value="1" />
+          <el-option label="2" value="2" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select placeholder="三级分类">
+          <el-option label="1" value="1" />
+          <el-option label="2" value="2" />
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script lang="ts">
+export default {
+  name: "JCategory",
+};
+</script>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div>JAttr</div>
+  <div>
+    <Categoty></Categoty>
+    <el-card class="mt-20">
+      <el-button type="primary" :icon="Plus">添加属性</el-button>
+      <el-table :data="tableData" stripe style="width: 100%" class="mt-20">
+        <el-table-column prop="date" label="Date" width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" />
+        <el-table-column label="操作">
+          <template #default="{ row }">
+            <el-button type="warning" :icon="Edit" class="options-btn"></el-button>
+            <el-button type="danger" :icon="Delete" class="options-btn"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,6 +24,36 @@ export default {
 };
 </script>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Categoty from "@/components/Category/index.vue";
+import { Delete, Edit, Plus } from "@element-plus/icons-vue";
+const tableData = [
+  {
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-02",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-04",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+  {
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
+  },
+];
+</script>
 
-<style scoped></style>
+<style scoped>
+.options-btn {
+  width: 30px;
+  height: 25px;
+}
+</style>
